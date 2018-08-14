07dcf46f5b7a4108fa459b9c250c438bacfef393
Miles <miles@Miless-MacBook-Pro.local>
2018-08-14 12:37:43 -0700

    alu/register transfer instruction

--- uasm/lexer.py
+++ uasm/lexer.py
@@ -4,14 +4,17 @@ tokens = (
     'VALUE_ID', 'LABEL_ID', 'SIGNAL_ID', 'NUMBER_DEC', 'NUMBER_HEX'
 )
 
-t_VALUE_ID = r' [%][0-9a-zA-Z_]+ '
-t_LABEL_ID = r' [@][0-9a-zA-Z_]+ '
-t_SIGNAL_ID = r' [a-zA-Z_][0-9a-zA-Z_]* '
-t_NUMBER_DEC = r' [0]|[1-9][0-9]* '
-t_NUMBER_HEX = r' [0][xX][0-9a-fA-F]+ '
+t_VALUE_ID = r'[%][0-9a-zA-Z_]+'
+t_LABEL_ID = r'[@][0-9a-zA-Z_]+'
+t_SIGNAL_ID = r'[a-zA-Z_][0-9a-zA-Z_]*'
+t_NUMBER_DEC = r'[0]|[1-9][0-9]*'
+t_NUMBER_HEX = r'[0][xX][0-9a-fA-F]+'
 
 literals = [';', ':', ',', '<', '>', '(', ')']
 t_ignore = ' \t'
+
+def t_inline_comment(t):
+    r'//[^\n]*'
 
 def t_newline(t):
     r'\n'
@@ -19,7 +22,7 @@ def t_newline(t):
 
 def t_error(t):
     print("illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
+    exit(1)
 
 # build lexer
 lexer = lex.lex()
